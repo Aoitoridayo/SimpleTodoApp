2645243a8c12ae2747f59c38a8987b8312dd659e
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-10-30 21:57:24 +0900

    Initial Commit

---
+++ SimpleTodoApp/SimpleTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoAppApp.swift
+//  SimpleTodoApp
+//
+//  Created by 渡邊魁優 on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
